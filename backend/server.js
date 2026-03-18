21b2601f6e6cfadb68b577097c8d9bddc80951f3
Dariia Vynokurova <dariiavynokurova@gmail.com>
2026-03-18 14:21:29 +0100

    fix homepage

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL);
@@ -293,12 +292,11 @@ START SERVER
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(process.cwd())));
+app.use(express.static(path.join(__dirname, "..")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "index.html"));
+  res.sendFile(path.join(__dirname, "..", "index.html"));
 });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
