f66eb9bb200a6569a112a07f4af497e26c4e37fd
Dariia Vynokurova <dariiavynokurova@gmail.com>
2026-03-18 14:06:53 +0100

    fix homepage

--- backend/server.js
+++ backend/server.js
@@ -287,7 +287,10 @@ START SERVER
 ====================== */
 
 const PORT = process.env.PORT || 3000;
-
+app.use(express.static("../"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("../index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
